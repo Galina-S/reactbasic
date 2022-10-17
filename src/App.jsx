1c1e6d77d01199c5852299af9246cc8fdbb86144
Galina Sazonova <galyna.sazonova@gmail.com>
2022-10-17 20:06:09 +0200

    basic style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import './App.css'
+import './App.scss'
+
+import  germanNouns from './data/germanNouns.json'
 
 function App() {
   return (
     <div className="App">
     <h1>Test site</h1>
-    <p>This is a test.</p>
+    <h2>German Nouns</h2>
+    <p>There are {germanNouns.length} German nouns.</p>
+    <div className="germanNouns">
+    {germanNouns.map(germanNoun => {
+      return (
+        <div className="germanNoun">
+        <div>{germanNoun.singular}</div>
+        </div>
+      )
+    })}
+    </div>
     </div>
   )
 }
