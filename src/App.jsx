f45e7bd8f745c597b8d7de24218f38df1ad53c9d
Galina Sazonova <galyna.sazonova@gmail.com>
2022-10-17 21:10:02 +0200

    GermanNouns

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
 import './App.scss'
-
-import  _germanNouns from './data/germanNouns.json'
-
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-  while (currentIndex != 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-  return array;
-}
-
-const germanNouns = shuffle(_germanNouns) //.filter(m => m.article === "das"); //
+import {GermanNouns} from './components/GermanNouns';
 
 function App() {
   return (
     <div className="App">
     <h1>Test site</h1>
-    <h2>German Nouns</h2>
-    <p>There are {germanNouns.length} German nouns.</p>
-    <div className="germanNouns">
-    {germanNouns.map(germanNoun => {
-      return (
-        <div className="germanNoun">
-          <img src="images/cardback.png" alt="" />
-          <div className="info">
-        <div className='singular'>{germanNoun.article} {germanNoun.singular}</div>
-        <div className='plural'>{germanNoun.plural}</div>
-        </div>
-        </div>
-      )
-    })}
+    <GermanNouns article="die"/>  
+    <GermanNouns article="der"/>  
+    <GermanNouns article="das"/>  
     </div>
-    </div>
-  )
+  );
 }
-
 export default App
